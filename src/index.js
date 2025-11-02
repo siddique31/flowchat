138a9ca3fee03dcf09e10673c9d3123cf6acb751
siddique31 <hafizsiddique531@gmail.com>
2025-11-02 13:33:23 +0500

    index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./style.css"; // ✅ Make sure this path is correct
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
